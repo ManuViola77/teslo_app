eb478e3b8a95f6e27871b94832e50c6e627f361a
Manuela Viola <manuela.viola@gmail.com>
2025-06-05 14:45:58 -0300

    feat: implement login and errors classes

--- lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
+++ lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
@@ -1,6 +1,11 @@
+import 'package:dio/dio.dart';
+import 'package:teslo_shop/config/config.dart';
 import 'package:teslo_shop/features/auth/domain/domain.dart';
 
+import '../infrastructure.dart';
+
 class AuthDatasourceImpl extends AuthDatasource {
+  final dio = Dio(BaseOptions(baseUrl: Environment.apiUrl));
   @override
   Future<User> checkAuthStatus(String token) {
     // TODO: implement checkAuthStatus
@@ -8,9 +13,18 @@ class AuthDatasourceImpl extends AuthDatasource {
   }
 
   @override
-  Future<User> login(String email, String password) {
-    // TODO: implement login
-    throw UnimplementedError();
+  Future<User> login(String email, String password) async {
+    try {
+      final response = await dio.post('auth/login', data: {
+        'email': email,
+        'password': password,
+      });
+
+      final user = UserMapper.userJsonToEntity(response.data);
+      return user;
+    } catch (e) {
+      throw WrongCredentials();
+    }
   }
 
   @override
