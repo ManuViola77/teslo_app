99f4e7263fdf6ea52e3695c1c3eba6ef3546c56d
Manuela Viola <manuela.viola@gmail.com>
2025-06-10 10:22:59 -0300

    feat: riverpod products notifier

--- lib/features/products/infrastructure/providers/products_provider.dart
+++ lib/features/products/infrastructure/providers/products_provider.dart
@@ -1,4 +1,37 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:teslo_shop/features/products/domain/domain.dart';
+
+class ProductsNotifier extends StateNotifier<ProductsState> {
+  final ProductsRepository productsRepository;
+
+  ProductsNotifier({required this.productsRepository})
+      : super(ProductsState()) {
+    loadNextPage();
+  }
+
+  Future<void> loadNextPage() async {
+    if (state.isLoading || state.isLastPage) return;
+
+    state = state.copyWith(isLoading: true);
+
+    final products = await productsRepository.getProductsByPage(
+      limit: state.limit,
+      offset: state.offset,
+    );
+
+    if (products.isEmpty) {
+      state = state.copyWith(isLoading: false, isLastPage: true);
+      return;
+    }
+
+    state = state.copyWith(
+      isLoading: false,
+      isLastPage: false,
+      offset: state.offset + state.limit,
+      products: [...state.products, ...products],
+    );
+  }
+}
 
 class ProductsState {
   final bool isLastPage;
