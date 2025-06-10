17888d3042ade92aa155d6a71c893fb8045a7242
Manuela Viola <manuela.viola@gmail.com>
2025-06-10 11:59:22 -0300

    feat: show products list without styles

--- lib/features/products/presentation/screens/products_screen.dart
+++ lib/features/products/presentation/screens/products_screen.dart
@@ -1,41 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:teslo_shop/features/shared/shared.dart';
+
+import '../../infrastructure/providers/providers.dart';
 
 class ProductsScreen extends StatelessWidget {
   const ProductsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     final scaffoldKey = GlobalKey<ScaffoldState>();
 
     return Scaffold(
-      drawer: SideMenu( scaffoldKey: scaffoldKey ),
+      drawer: SideMenu(scaffoldKey: scaffoldKey),
       appBar: AppBar(
         title: const Text('Products'),
         actions: [
-          IconButton(
-            onPressed: (){}, 
-            icon: const Icon( Icons.search_rounded)
-          )
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search_rounded))
         ],
       ),
       body: const _ProductsView(),
       floatingActionButton: FloatingActionButton.extended(
         label: const Text('Nuevo producto'),
-        icon: const Icon( Icons.add ),
+        icon: const Icon(Icons.add),
         onPressed: () {},
       ),
     );
   }
 }
 
-
-class _ProductsView extends StatelessWidget {
+class _ProductsView extends ConsumerStatefulWidget {
   const _ProductsView();
 
   @override
+  _ProductsViewState createState() => _ProductsViewState();
+}
+
+class _ProductsViewState extends ConsumerState<_ProductsView> {
+  final ScrollController scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController.addListener(onScroll);
+    ref.read(productsProvider.notifier).loadNextPage();
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
+  void onScroll() {
+    /* if (scrollController.position.pixels >= scrollController.position.maxScrollExtent - 500) {
+      ref.read(productsProvider.notifier).loadNextPage();
+    } */
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Eres genial!'));
+    final products = ref.watch(productsProvider).products;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: MasonryGridView.count(
+        physics: const BouncingScrollPhysics(),
+        crossAxisCount: 2,
+        mainAxisSpacing: 20,
+        crossAxisSpacing: 35,
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          final product = products[index];
+          return Text(product.title);
+        },
+      ),
+    );
   }
 }
