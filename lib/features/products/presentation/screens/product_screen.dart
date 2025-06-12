1e3dccc43f6c84506cff200fb08bfa7ceb081c56
Manuela Viola <manuela.viola@gmail.com>
2025-06-12 00:29:19 -0300

    fix: hide keyboard on tapping outside

--- lib/features/products/presentation/screens/product_screen.dart
+++ lib/features/products/presentation/screens/product_screen.dart
@@ -66,36 +66,39 @@ class ProductScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final productState = ref.watch(productProvider(productId));
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Editar Producto'),
-        actions: [
-          IconButton(
-              onPressed: () {}, icon: const Icon(Icons.camera_alt_outlined))
-        ],
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Editar Producto'),
+          actions: [
+            IconButton(
+                onPressed: () {}, icon: const Icon(Icons.camera_alt_outlined))
+          ],
+        ),
+        body: productState.isLoading
+            ? const FullScreenLoader()
+            : _ProductView(product: productState.product!),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              if (productState.product == null) return;
+
+              ref
+                  .read(productFormProvider(productState.product!).notifier)
+                  .onFormSubmit()
+                  .then((value) {
+                if (!value || !context.mounted) return;
+
+                showCenterOverlay(
+                    context,
+                    (productState.product!.id == 'new')
+                        ? 'Producto creado correctamente'
+                        : 'Producto actualizado correctamente');
+                context.pop();
+              });
+            },
+            child: const Icon(Icons.save_as_outlined)),
       ),
-      body: productState.isLoading
-          ? const FullScreenLoader()
-          : _ProductView(product: productState.product!),
-      floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            if (productState.product == null) return;
-
-            ref
-                .read(productFormProvider(productState.product!).notifier)
-                .onFormSubmit()
-                .then((value) {
-              if (!value || !context.mounted) return;
-
-              showCenterOverlay(
-                  context,
-                  (productState.product!.id == 'new')
-                      ? 'Producto creado correctamente'
-                      : 'Producto actualizado correctamente');
-              context.pop();
-            });
-          },
-          child: const Icon(Icons.save_as_outlined)),
     );
   }
 }
@@ -242,6 +245,7 @@ class _SizeSelector extends StatelessWidget {
       }).toList(),
       selected: Set.from(selectedSizes),
       onSelectionChanged: (newSelection) {
+        FocusScope.of(context).unfocus();
         onSizesChanged(List.from(newSelection));
       },
       multiSelectionEnabled: true,
@@ -279,6 +283,7 @@ class _GenderSelector extends StatelessWidget {
         }).toList(),
         selected: {selectedGender},
         onSelectionChanged: (newSelection) {
+          FocusScope.of(context).unfocus();
           onGenderChanged(newSelection.first);
         },
       ),
